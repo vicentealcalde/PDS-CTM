49fa56d87741ff99c5589c69b520c3406274b119
Vicente A <valcalde@miuandes.cl>
2022-10-16 11:45:27 -0300

    fix: change render of user

--- app/src/main/java/cl/uandes/pichangapp/views/organized_match/OrganizedMatchAdapter.kt
+++ app/src/main/java/cl/uandes/pichangapp/views/organized_match/OrganizedMatchAdapter.kt
@@ -17,30 +17,26 @@ class OrganizedMatchAdapter (
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val profileImage = itemView.findViewById<ImageView>(R.id.profile_image)!!
-        val textView2 = itemView.findViewById<TextView>(R.id.textView2)!!
+
+
         val textView4 = itemView.findViewById<TextView>(R.id.textView4)!!
-        val textView6= itemView.findViewById<TextView>(R.id.textView6)!!
+
         val button = itemView.findViewById<ViewGroup>(R.id.linearLayoutPi)!!
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OrganizedMatchAdapter.ViewHolder {
         val context = parent.context
         val inflater = LayoutInflater.from(context)
-        val resultView: View = inflater.inflate(R.layout.xx_extra, parent, false)
+        val resultView: View = inflater.inflate(R.layout.expand_add_friend, parent, false)
         return ViewHolder(resultView)
     }
 
     override fun onBindViewHolder(holder: OrganizedMatchAdapter.ViewHolder, position: Int) {
 
         val match: Match = matches[position]
-        val textView2 = holder.textView2
         val textView4 = holder.textView4
-        val textView6 = holder.textView6
         val profileImage = holder.profileImage
         val button = holder.button
-
-        textView2.text = match.DayOfMatch
-        textView6.text = match.LocationOfMatch
         textView4.text = match.HourOfMatch
 
         button.setOnClickListener {
